3b3f63ca5aa816971b147a8aac376389bc41e12e
bergerst <stefan.berger@x-tention.at>
2017-01-12 15:42:59 +0100

    Fixed run.bat files for samples
    The class paths were set incorrectly and !BALANA_CLASSPATH! is only evaluated if delayed expansion is enabled.

--- modules/balana-samples/hierarchical-resource/run.bat
+++ modules/balana-samples/hierarchical-resource/run.bat
@@ -1,8 +1,9 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 set BALANA_CLASSPATH=.\conf
-FOR %%C in ("\lib\*.jar") DO set BALANA_CLASSPATH=!BALANA_CLASSPATH!;".\lib\%%~nC%%~xC"
-FOR %%D in ("\..\..\balana-core\target\*.jar") DO set BALANA_CLASSPATH=!BALANA_CLASSPATH!;".\..\..\balana-core\target\%%~nD%%~xD"
-FOR %%E in ("\target\*.jar") DO set BALANA_CLASSPATH=!BALANA_CLASSPATH!;".\target\%%~nE%%~xE"
+FOR %%C in (".\..\lib\*.jar") DO set BALANA_CLASSPATH=!BALANA_CLASSPATH!;".\..\lib\%%~nC%%~xC"
+FOR %%D in (".\..\..\balana-core\target\*.jar") DO set BALANA_CLASSPATH=!BALANA_CLASSPATH!;".\..\..\balana-core\target\%%~nD%%~xD"
+FOR %%E in (".\target\*.jar") DO set BALANA_CLASSPATH=!BALANA_CLASSPATH!;".\target\%%~nE%%~xE"
 set _RUNJAVA="%JAVA_HOME%\bin\java"
 %_RUNJAVA% -cp "%BALANA_CLASSPATH%" org.wso2.balana.samples.hierarchical.resource.Main %*
 
